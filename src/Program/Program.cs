b6dd67e27b398a23e35ce19be08cbe108b4535ee
Valentin <108636550+Valentin12000@users.noreply.github.com>
2026-08-11 11:01:33 -0300

    Revert "before revert"
    This reverts commit 9616ab7e444cc44c24b40e3fa9ea795ee1cccbd0.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,7 +4,7 @@
     {
         Console.WriteLine(Addition.Add(1, 2));
         Console.WriteLine(Subtraction.Subtract(3, 4));
-        // Console.WriteLine(Multiplication.Multiply(5, 6));
+        Console.WriteLine(Multiplication.Multiply(5, 6));
         Console.WriteLine(Division.Divide(7, 8));
     }
 }
