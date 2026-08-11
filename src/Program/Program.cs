2573867985fbf1f5fe1cd3d66d487740fb06f537
Valentin <108636550+Valentin12000@users.noreply.github.com>
2026-08-11 11:00:43 -0300

    ermmmm

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,9 +7,7 @@
         // Console.WriteLine(Multiplication.Multiply(5, 6));
         Console.WriteLine(Division.Divide(7, 8));
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    }
+}
     
 public class Division
 {
@@ -18,15 +16,8 @@ public class Division
        return (double)a / b;
    }
 }
-=======
-}
-
-// Esta clase implementa la operación <nombre-operación>
-=======
-}
 
 // Esta clase implementa la operación suma
->>>>>>> dc6c1e9095c1824b9dfbea0329dc3cc4c15a2965
 public class Addition
 {
    public static int Add(int a, int b)
@@ -34,9 +25,6 @@ public class Addition
        return 0;//a + b;
    }
 }
-<<<<<<< HEAD
->>>>>>> 1b5fb21ef5927fb483971a8b14b02c6933a29a5a
-=======
 
 // Esta clase implementa la operación resta
 public class Subtraction
@@ -46,4 +34,3 @@ public class Subtraction
        return a - b;
    }
 }
->>>>>>> dc6c1e9095c1824b9dfbea0329dc3cc4c15a2965
